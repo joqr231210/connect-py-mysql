cee15a0c84291c49272c9bc1ddb51d8c25a0d1b7
joqr231210 <joqr231210@gmail.com>
2022-03-11 11:09:20 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import mysql.connector
+
+cnx = mysql.connector.connect(user='root',
+                              password='Futuro2020',
+                              host='localhost',
+                              database='sygnalys')
+cursor = cnx.cursor()
+
+add_telemetry = ("INSERT INTO telemetry"
+                 "(id, sensor, sn, gtw, vx)"
+                 "VALUES (%s, %s, %s, %s, %s)")
+
+data_telemetry = (6, 'vph-11', '9', 'g001', 2.5)
+
+cursor.execute(add_telemetry, data_telemetry)
+
+# query = "SELECT sensor, sn, gtw, vx FROM telemetry WHERE sn = '1'"
+
+# cursor.execute(query)
+for (sensor) in cursor:
+    print(sensor)
+
+cnx.commit()
+
+cursor.close()
+cnx.close()
